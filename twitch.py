da078b5cddd2a30f8e807a554d61dbb2461bdd89
Steve Jaoudi <sjaoudi17@gmail.com>
2015-09-01 21:37:49 -0400

    polarity/subjectivity detection for each message

--- twitch.py
+++ twitch.py
@@ -5,7 +5,7 @@ HOST = 'irc.twitch.tv'
 PORT = 6667
 NICK = 'sjaoudi'
 PASS = 'oauth:03nj467om5d8sm9dgrllor2a97zie4'
-CHAN = '#voyboy'
+CHAN = '#tsm_theoddone'
 
 s = socket.socket()
 s.connect((HOST, PORT))
@@ -13,7 +13,8 @@ s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
 s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
 s.send("JOIN {}\r\n".format(CHAN).encode("utf-8"))
 
-CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+# CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+CHAT_MSG=re.compile(r":\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 
 # blob = TextBlob('happy')
 # print(blob.sentences[0].sentiment)
@@ -23,7 +24,11 @@ while True:
     if response == "PING :tmi.twitch.tv\r\n":
         s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
     else:
-        #print(response)
-
+        # print(response)
         message = CHAT_MSG.sub('', response)
-        print(message)
+
+        blob = TextBlob(message)
+        print(message, blob.sentences[0].sentiment)
+        # print(blob.sentences[0].sentiment)
+
+        # print(message)
